a6297861d9ab0314d313d241d2cdddea1fdf070c
Blaser993 <corradodepinto.design@gmail.com>
2023-10-12 11:07:31 +0200

    fotoformmodel crud

--- net-il-mio-fotoalbum/Controllers/FotoController.cs
+++ net-il-mio-fotoalbum/Controllers/FotoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using net_il_mio_fotoalbum.Database;
 using net_il_mio_fotoalbum.Models;
 
@@ -12,7 +14,7 @@ namespace net_il_mio_fotoalbum.Controllers
             using(FotoContext db = new FotoContext())
             {
 
-                List<Foto> fotos = db.Fotos.ToList<Foto>();
+                List<Foto> fotos = db.Fotos.Include(foto => foto.Categories).ToList<Foto>();
 
 
                 return View("index", fotos);
@@ -22,13 +24,64 @@ namespace net_il_mio_fotoalbum.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Foto foto)
+        public IActionResult Create(FotoFormModel data)
         { 
             if(!ModelState.IsValid)
             {
-                return View("Create", foto);
+                using (FotoContext db = new FotoContext())
+                {
+
+                List<SelectListItem> allCategorySelectList = new List<SelectListItem>();
+                List<Category> databaseCategories = db.Categories.ToList();
+
+
+                    foreach(Category category in databaseCategories)
+                    {
+                        allCategorySelectList.Add(new SelectListItem()
+                        {
+                            Text = category.Name,
+                            Value = category.CategoryId.ToString(),
+                        });
+                    }
+                    data.Categories = allCategorySelectList;
+
+                    return View("Create", data);
+
+                }
             }
 
+            if (data.Foto == null)
+            {
+                data.Foto = new Foto(); // inizializza l'oggetto Foto se è null
+            }
+
+            if (data.Foto.Categories == null)
+            {
+                data.Foto.Categories = new List<Category>(); // inizializzare la lista di ingredienti se è null
+            }
+
+
+            if(data.SelectedCategoriesId != null)
+            {
+                using (FotoContext db = new FotoContext())
+                {
+                    foreach (string categorySelectedId in data.SelectedCategoriesId)
+                    {
+                        int intCategoriesSelectId = int.Parse(categorySelectedId);
+
+                        Category? categoryInDb = db.Categories.Where(category => category.CategoryId == intCategoriesSelectId).FirstOrDefault();
+
+                        if (categoryInDb != null)
+                        {
+                            data.Foto.Categories.Add(categoryInDb);
+                        }                           
+                    }
+
+                    db.Fotos.Add(data.Foto);
+                    db.SaveChanges();                  
+                }                
+            }
+            /*
             using (FotoContext  db = new FotoContext())
             {
                 Foto fotoToCreate = new Foto();
@@ -43,12 +96,41 @@ namespace net_il_mio_fotoalbum.Controllers
 
                 return RedirectToAction("Index");
             }
+            */
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
         public IActionResult Create()
         {
-            return View("Create");
+            using (FotoContext db = new FotoContext())
+            {
+
+                List<Foto> fotos = db.Fotos.ToList<Foto>();
+
+
+                List<SelectListItem> allCategorySelectList = new List<SelectListItem>();
+                List<Category> databaseCategories = db.Categories.ToList();
+
+                foreach (Category category in databaseCategories)
+                {
+                    allCategorySelectList.Add(
+                        new SelectListItem
+                        {
+                            Text = category.Name,
+                            Value = category.CategoryId.ToString()
+                        });
+                }
+
+                FotoFormModel model = new FotoFormModel
+                {
+                    Foto = new Foto(),
+                    Categories = allCategorySelectList
+                };
+
+                return View("Create", model);
+
+            }
         }
 
         [HttpGet]
@@ -56,7 +138,7 @@ namespace net_il_mio_fotoalbum.Controllers
         {
             using (FotoContext db = new FotoContext())
             {
-                Foto fotoToEdit = db.Fotos.Where(foto => foto.FotoId == id).FirstOrDefault();
+                Foto? fotoToEdit = db.Fotos.Where(foto => foto.FotoId == id).Include(foto => foto.Categories).FirstOrDefault();
 
                 if (fotoToEdit == null)
                 {
@@ -64,31 +146,92 @@ namespace net_il_mio_fotoalbum.Controllers
                 }
                 else
                 {
-                    return View(fotoToEdit);
+                    List<Category> dbCategoryList = db.Categories.ToList();
+                    List<SelectListItem> selectListItem = new List<SelectListItem>(); 
+
+                    foreach (Category category in dbCategoryList)
+                    {
+                        selectListItem.Add(new SelectListItem
+                        { 
+                        Value = category.CategoryId.ToString(), 
+                        Text = category.Name,
+                        Selected = fotoToEdit.Categories.Any(tagAssociated => tagAssociated.CategoryId == category.CategoryId)
+                        });
+                    }
+
+                    FotoFormModel model =
+                        new FotoFormModel { Foto = fotoToEdit, Categories = selectListItem };
+                    return View("Update",model);
                 }
             }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Update(int id, Foto foto)
+        public IActionResult Update(int id, FotoFormModel data)
         {
             if (!ModelState.IsValid)
             {
-                return View("Update", foto);
+                using (FotoContext db = new FotoContext())
+                {
+                    List<Category> categories = db.Categories.ToList();
+                    List<SelectListItem> selectListItem = new List<SelectListItem>();
+
+                    foreach (Category category in categories)
+                    {
+                        selectListItem.Add(new SelectListItem
+                        {
+                            Value =category.CategoryId.ToString(),
+                            Text = category.Name,
+                        });
+                    }
+
+                    data.Categories = selectListItem;
+
+                    return View("Update", data);
+                }
+
+                
             }
 
             using (FotoContext db = new FotoContext())
             {
-                Foto fotoToUpdate = db.Fotos.Where(foto => foto.FotoId == id).FirstOrDefault();
+                Foto? fotoToUpdate = db.Fotos
+                    .Where(foto => foto.FotoId == id)
+                    .Include(foto => foto.Categories)
+                    .FirstOrDefault();
 
-                if(fotoToUpdate != null)
+                if (fotoToUpdate != null)
                 {
-                    fotoToUpdate.Title = foto.Title;
-                    fotoToUpdate.Image = foto.Image;
-                    fotoToUpdate.Description = foto.Description;
-                    fotoToUpdate.Visibility = foto.Visibility;
-                    fotoToUpdate.Categories = foto.Categories;
+                    fotoToUpdate.Categories.Clear();
+
+                    fotoToUpdate.Title = data.Foto.Title;
+                    fotoToUpdate.Image = data.Foto.Image;
+                    fotoToUpdate.Description = data.Foto.Description;
+                    fotoToUpdate.Visibility = data.Foto.Visibility;
+
+                    // Verifica se data.Foto.Categories è null e, se non lo è, assegnalo a fotoToUpdate.Categories
+                    if (data.Foto.Categories != null)
+                    {
+                        fotoToUpdate.Categories = data.Foto.Categories;
+                    }
+
+                    if (data.SelectedCategoriesId != null)
+                    {
+                        foreach (string categorySelectedId in data.SelectedCategoriesId)
+                        {
+                            int intCategorySelectedId = int.Parse(categorySelectedId);
+
+                            Category? categoryInDb = db.Categories
+                                .Where(category => category.CategoryId == intCategorySelectedId)
+                                .FirstOrDefault();
+
+                            if (categoryInDb != null)
+                            {
+                                fotoToUpdate.Categories.Add(categoryInDb);
+                            }
+                        }
+                    }
 
                     db.SaveChanges();
 
